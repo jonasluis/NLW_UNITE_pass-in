6a55a6d847cb86355f092999df2cc5251d54b220
jonasluis <144178301+jonasluis@users.noreply.github.com>
2024-04-02 20:23:15 -0300

    Segunda Aula NLW UNITE ROCKETSEAT Criado toda estrutura para utilizar a api insomnia

---
+++ src/main/java/rocketseat/com/passin/dto/event/EventResponseDTO.java
@@ -0,0 +1,16 @@
+package rocketseat.com.passin.dto.event;
+
+import lombok.Getter;
+
+import rocketseat.com.passin.domain.event.Event;
+
+@Getter
+
+public class EventResponseDTO {
+
+    EventDetailDto event;
+
+    public EventResponseDTO(Event event, Integer numberOfAttendees){
+        this.event = new EventDetailDto(event.getId(), event.getTitle(), event.getDetails(), event.getSlug(), event.getMaximumAttendees(), numberOfAttendees);
+    }
+}
